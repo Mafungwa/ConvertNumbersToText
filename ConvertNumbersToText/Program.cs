2d1e1dc28ff5d4362c2900a48829eed15721adc7
Mafungwa <33479523+Mafungwa@users.noreply.github.com>
2023-12-12 14:56:35 +0200

    first commit

---
+++ ConvertNumbersToText/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConvertNumbersToText
+{
+    internal class Program
+    {
+       // private static int readline;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please Enter any number not more than 4 digit or X to exit");
+
+            string a = "x";
+
+            string b;
+            String[] ab = new string[4]; 
+
+            //Console.WriteLine(a); 
+            
+
+            if (a != "X")
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    string number = Console.ReadLine();
+                    //ab = number;
+
+                    //Console.WriteLine(number); 
+                    Console.WriteLine(a);
+                    i++;
+                }
+                
+            }
+            
+            else
+            {
+                Console.WriteLine("Goodbye");
+            }
+            
+
+        }
+    }
+}
